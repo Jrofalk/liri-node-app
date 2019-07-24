f510b80d6f8653cd32b1c664d8398cf7ea3b9bd3
John Falk <jrfalk@outlook.com>
2019-07-24 14:28:29 -0400

    Created initial switch case statements which direct which function to run depending on user command

--- liri.js
+++ liri.js
@@ -2,3 +2,25 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+var commands = process.argv[2];
+var value = process.argv[3];
+
+switch (commands) {
+case "concert-this":
+  //each function goes on this line ex. deposit();
+  break;
+
+case "spotify-this-song":
+  
+  break;
+
+case "movie-this":
+  
+  break;
+
+case "do-what-it-says":
+  
+  break;
+}
+
