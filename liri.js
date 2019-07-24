eb380156d70c6fa15ba0cdc07ec5830f822bc5e2
John Falk <jrfalk@outlook.com>
2019-07-24 15:33:59 -0400

    Created for loop that displays venue name and location for an artist that is entered as arguement in command line

--- liri.js
+++ liri.js
@@ -9,29 +9,35 @@ var commands = process.argv[2];
 var value = process.argv[3];
 
 switch (commands) {
-case "concert-this":
-  concertThis();
-  break;
+    case "concert-this":
+        concertThis();
+        break;
 
-case "spotify-this-song":
-  
-  break;
+    case "spotify-this-song":
 
-case "movie-this":
-  
-  break;
+        break;
 
-case "do-what-it-says":
-  
-  break;
+    case "movie-this":
+
+        break;
+
+    case "do-what-it-says":
+
+        break;
 }
 
-function concertThis(){
+function concertThis() {
     var artist = value;
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-        function(response) {
-          console.log(response.data);
+        function (response) {
+            let results = response.data;
+            console.log(results);
+            for(let i = 0; i < results.length; i++){
+                console.log("Venue: " + results[i].venue.name);
+                console.log("Venue Location: " + results[i].venue.city + ", " + results[i].venue.region + " " + results[i].venue.country);
+            }
+
         }
-      );
+    );
 }
 
