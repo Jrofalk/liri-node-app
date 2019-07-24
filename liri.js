a128f6f0e4b77eae5978351fc7a009b66220f647
John Falk <jrfalk@outlook.com>
2019-07-24 17:02:36 -0400

    Added function to search spotify api for a given song. Created loop that displays song title and the artists name

--- liri.js
+++ liri.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 
+var Spotify = require('node-spotify-api');
 var axios = require("axios");
-//var keys = require("./keys.js");
-//var spotify = new Spotify(keys.spotify);
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
 
 
 var commands = process.argv[2];
@@ -14,7 +15,7 @@ switch (commands) {
         break;
 
     case "spotify-this-song":
-
+        spotifySong();
         break;
 
     case "movie-this":
@@ -32,13 +33,31 @@ function concertThis() {
         function (response) {
             let results = response.data;
             console.log(results);
-            for(let i = 0; i < results.length; i++){
+            for (let i = 0; i < results.length; i++) {
                 console.log("Venue: " + results[i].venue.name);
                 console.log("Venue Location: " + results[i].venue.city + ", " + results[i].venue.region + " " + results[i].venue.country);
+                //Will need to add moment js to what is below in order to translate date into proper format, look at inquirergeocode.js from activities to see how to do this
                 console.log("Date: " + results[i].datetime);
+            }
+        });
+}
+
+function spotifySong() {
+    var song = value;
+    spotify.search({ type: 'track', query: song, limit: 1 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        else {
+            let results = data.tracks.items;
+            console.log(results);
+            for (let i = 0; i < results.length; i++) {
+                console.log("Artist(s): " + results[i].artists[0].name);
+                console.log("Song: " + results[i].name);
             }
 
         }
-    );
+    });
 }
+
 
